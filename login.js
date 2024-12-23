1cbda49f431f55d101892065ed130e19f173d535
Kolya <160027167+SweetieKitty@users.noreply.github.com>
2024-12-23 09:24:37 +0200

    Update login.js

--- login.js
+++ login.js
@@ -33,12 +33,12 @@ verifyButton.addEventListener('click', () => {
     backButton.style.display = 'flex';
 });
 document.getElementById('backButton').addEventListener('click', function() {
-    window.location.href = 'login.html'; 
+    window.location.href = 'sait.html'; 
 });
     
     
 document.getElementById('nextButton1').addEventListener('click', function() {
-    window.location.href = '../index.html'; 
+    window.location.href = '../sait.html'; 
 });
 
 
@@ -47,28 +47,4 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const apiUrl = "https://jsonplaceholder.typicode.com/posts"; // тестовий API
 
-  // Запит до API
-  fetch(apiUrl)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error("Помилка завантаження даних");
-      }
-      return response.json();
-    })
-    .then(data => {
-      // Виведення даних на сторінку
-      data.slice(0, 10).forEach(item => { // виводимо перші 10 елементів
-        const card = document.createElement("div");
-        card.className = "catalog-item";
-        card.innerHTML = `
-          <h3>${item.title}</h3>
-          <p>${item.body}</p>
-        `;
-        container.appendChild(card);
-      });
-    })
-    .catch(error => {
-      console.error("Помилка:", error);
-      container.innerHTML = "<p>Не вдалося завантажити дані</p>";
-    });
-});
+
